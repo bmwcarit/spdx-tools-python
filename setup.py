38d931bd581b6168e2f14e47f99aeed950e2a6c3
Hugo Jacob <Hugo.Jacob@bmw.de>
2016-11-25 11:59:03 +0000

    setup: change version to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from distutils.core import setup
 setup(name='spdx',
-      version='0.1',
+      version='0.2',
       description='SPDX tag/value and RDF tools',
       packages=['spdx', 'spdx.parsers', 'spdx.writers', 'spdx.parsers.lexers'],
       author='Ahmed H. Ismail',
